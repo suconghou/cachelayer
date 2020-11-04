1b22e67d5c59a333c34225972311c314f6bd4dd3
suconghou <1126045770@qq.com>
2020-11-04 16:51:03 +0800

    improve

--- request/request.go
+++ request/request.go
@@ -1,13 +1,19 @@
 package request
 
 import (
+	"crypto/tls"
 	"io"
 	"net/http"
 	"time"
 )
 
 var (
-	client = &http.Client{Timeout: time.Minute * 2}
+	client = &http.Client{
+		Timeout: time.Minute * 2,
+		Transport: &http.Transport{
+			TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
+		},
+	}
 )
 
 // Req request return http.Respsone
